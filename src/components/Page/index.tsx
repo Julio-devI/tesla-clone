1a07c5d25ea25d0052d0e7b65fbfe10d01b9a28e
julio <julioakzzo@hotmail.com>
2021-11-22 18:00:06 -0300

    first commit

---
+++ src/components/Page/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import { ModelsWrapper, ModelSection } from '../Model'
+import DefaultOverlayContent from '../DefaultOverlayContent';
+
+import { Container } from './styles'
+
+const Page: React.FC = () => {
+    return (
+        <Container>
+            <ModelsWrapper>
+                <div>
+                    <ModelSection
+                        modelName="Model One"
+                        overlayNode={
+                            <DefaultOverlayContent
+                                label="Model One"
+                                description="Order Online for Delivery"
+                            />
+                        }
+                    />
+                </div>
+            </ModelsWrapper>
+        </Container>
+    );
+};
+
+export default Page;
---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+    * {
+        margin: 0px;
+        padding: 0px;
+        box-sizing: border-box;
+    }
+
+    *,input,button {
+        font-family: 'Roboto', sans-serif;
+    }
+`
