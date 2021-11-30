f89a4c2f46d9529c9e2726099bad736042241aee
julio <julioakzzo@hotmail.com>
2021-11-30 18:23:37 -0300

    model overlay

--- src/components/Model/ModelOverlay/index.tsx
+++ src/components/Model/ModelOverlay/index.tsx
@@ -1,16 +1,50 @@
-import React from 'react'
+import { useTransform } from 'framer-motion'
+import React, { useCallback, useLayoutEffect, useState } from 'react'
+
+import { CarModel } from '../ModelsContext'
 
 import useWrapperScroll from '../userWrapperScroll'
 
 import { Container } from './styles'
 
-const ModelOverlay: React.FC = ({ children }) => {
+interface Props {
+    model: CarModel
+}
+
+type SectionDimensions = Pick<HTMLDivElement, 'offsetTop' | 'offsetHeight'>
+
+const ModelOverlay: React.FC<Props> = ({ model, children }) => {
     const { scrollY } = useWrapperScroll()
 
-    return (
-        <Container>
-            {children}
-        </Container>
+    const getSectionDimensions = useCallback(() => {
+        return {
+            offsetTop: model.sectionRef.current?.offsetTop,
+            offsetHeight: model.sectionRef.current?.offsetHeight
+        } as SectionDimensions
+    }, [model.sectionRef])
+
+    const [dimensions, setDimensions] = useState<SectionDimensions>(
+        getSectionDimensions()
+    )
+
+    useLayoutEffect(() => {
+        function onResize() {
+            window.requestAnimationFrame(() => setDimensions(getSectionDimensions()))
+        }
+
+        window.addEventListener('resize', onResize)
+
+        return () => window.removeEventListener('resize', onResize)
+    }, [getSectionDimensions])
+
+    return <Container style={ }> {children} </Container>
+
+    const sectionScrollProgress = useTransform(scrollY, y => (y - dimensions.offsetTop) / dimensions.offsetHeight)
+
+    const opacity = useTransform(
+        sectionScrollProgress,
+        [-0.42, -0.05],
+        [0, 1]
     )
 }
 
--- src/components/Model/ModelsWrapper/index.tsx
+++ src/components/Model/ModelsWrapper/index.tsx
@@ -33,7 +33,7 @@ const ModelsWrapper: React.FC = ({ children }) => {
             <Container ref={wrapperRef}>
                 <OverlaysRoot>
                     {registeredModels.map(item => (
-                        <ModelOverlay key={item.modelName}>
+                        <ModelOverlay key={item.modelName} model={item}>
                             {item.overlayNode}
                         </ModelOverlay>
                     ))}
